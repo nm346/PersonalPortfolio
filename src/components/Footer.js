3c0b2370d1eb257bd8645d098df91a86da092d10
Nathan May <nathanmay@MacBook-Pro-M3.local>
2025-01-10 22:14:46 +0000

    added cv and some personal details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon3 from "../assets/img/cord.svg";
 
 export const Footer = () => {
   return (
@@ -14,9 +14,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/nathanmay22/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://github.com/nm346/"  target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Github" /></a>
+                <a href="https://cord.co/candidate/account/u/candidate/46772" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Cord" /></a>
             </div>
             <p>Copyright 2025. All Rights Reserved</p>
           </Col>
