5c7c5e741534c52af984c50d9698f0dba7be5a4f
Nathan May <nathanmay@MacBook-Pro-M3.local>
2025-01-10 18:19:25 +0000

    added footer and contact

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -42,7 +42,7 @@ export const Projects = () => {
                     <h2>Projects</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     <Tab.Container id="project-tabs" defaultActiveKey="first">
-                        <Nav variant="pills" defaultActiveKey="/home">
+                        <Nav variant="pills" className="nav-pills mb-5 justify-content align-items-center" id="pills-tab">
                         <Nav.Item>
                             <Nav.Link eventKey="first">Tab One</Nav.Link>
                         </Nav.Item>
