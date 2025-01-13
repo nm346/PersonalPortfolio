3db72c9c33b5c07d5d30ab7c08a684b7621186aa
Nathan May <nathanmay@MacBook-Pro-M3.local>
2025-01-13 20:09:07 +0000

    Projects section almost done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,98 +1,109 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
+import React, { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import projImg4 from "../assets/img/project-img3.png";
 
 export const Projects = () => {
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const projects = [
+  const projectGroups = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Special Project 1",
+      description: "This is a highlighted project showcasing unique design and development capabilities. Click below to learn more about it.",
+      buttonText: "Learn More",
+      images: [projImg1, projImg2, projImg3, projImg4],
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Special Project 2",
+      description: "Another unique project with a focus on cutting-edge technologies. Discover the features and innovations behind this work.",
+      buttonText: "Explore More",
+      images: [projImg3, projImg4, projImg1, projImg2],
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "Special Project 3",
+      description: "A groundbreaking project that explores new horizons in technology and design. See how it makes a difference.",
+      buttonText: "Check it Out",
+      images: [projImg2, projImg3, projImg4, projImg1],
     },
   ];
+
+  const currentGroup = projectGroups[currentPage - 1];
 
   return (
     <section className="project" id="projects">
       <Container>
+        {/* Title */}
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Throughout the last few years, I’ve worked on many side projects as a developer and tester, this ReactJs website is one of them but two more of my favourites.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
-            </TrackVisibility>
+            <h1 className="text-center">Projects</h1>
+          </Col>
+        </Row>
+        {/* Description */}
+        <Row className="justify-content-center">
+          <Col md={8} className="text-center">
+            <p>
+              Explore the projects I've worked on during my journey as a developer. 
+              These range from business applications to unique startup designs.
+            </p>
+          </Col>
+        </Row>
+        {/* Navigation Bar */}
+        <Row className="justify-content-center mb-4">          
+          
+          <div className="nav-buttons">
+          <Col md={2} className="text-center">
+
+            <Button
+              className={`project-nav-btn ${currentPage === 1 ? "active" : ""}`}
+              onClick={() => setCurrentPage(1)}
+            >
+              Button 1
+            </Button>
+          </Col>
+          <Col md={2} className="text-center">
+            <Button
+              className={`project-nav-btn ${currentPage === 2 ? "active" : ""}`}
+              onClick={() => setCurrentPage(2)}
+            >
+              Button 2
+            </Button>
+          </Col>
+          <Col md={2} className="text-center">
+            <Button
+              className={`project-nav-btn ${currentPage === 3 ? "active" : ""}`}
+              onClick={() => setCurrentPage(3)}
+            >
+              Button 3
+            </Button> 
+          </Col></div>
+        </Row>           
+
+        {/* Project Group */}
+        <Row>
+          <Col md={4} className="project-highlight">
+            <h2>{currentGroup.title}</h2>
+            <p>{currentGroup.description}</p>
+            <Button className="learn-more-btn">{currentGroup.buttonText}</Button>
+          </Col>
+          <Col md={8}>
+            <Row>
+              {currentGroup.images.map((image, index) => (
+                <Col md={6} key={index}>
+                  <div className="proj-imgbx">
+                    <img src={image} alt={`Project ${index + 1}`} />
+                    <div className="proj-txtx">
+                      <h4>Project {index + 1}</h4>
+                      <p>Description {index + 1}</p>
+                    </div>
+                  </div>
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
